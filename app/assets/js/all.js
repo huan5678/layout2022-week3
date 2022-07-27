027f00fabab3edb20f7d7973a3e9ee75aac85763
黃士桓 <huan5678@gmail.com>
2022-07-27 23:48:27 +0800

    [update] 初步完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,1 +1,6 @@
-$('.menu').on('click', () => {});
+$('.menu').on('click', () => {
+  $('.nav--list').toggleClass('none-sm');
+  setTimeout(() => {
+    $('.nav--list').toggleClass('active');
+  }, 300);
+});
